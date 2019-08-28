77722fbb3587caa85a35dde1fc1351212c04557e
Anika-Hossain-Orthy <52565535+Anika-Hossain-Orthy@users.noreply.github.com>
2019-08-29 03:43:50 +0600

    Add files via upload

--- autoload.php
+++ autoload.php
@@ -1,17 +1,26 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
+/**
+ * Simple autoloader that follow the PHP Standards Recommendation #0 (PSR-0)
+ * @see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md for more informations.
+ *
+ * Code inspired from the SplClassLoader RFC
+ * @see https://wiki.php.net/rfc/splclassloader#example_implementation
+ */
+spl_autoload_register(function ($className) {
+    $className = ltrim($className, '\\');
+    $fileName = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $className . '.php';
+    if (file_exists($fileName)) {
+        require $fileName;
 
-/*
-|--------------------------------------------------------------------------
-| Register The Composer Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for our application. We just need to utilize it! We'll require it
-| into the script here so that we do not have to worry about the
-| loading of any our classes "manually". Feels great to relax.
-|
-*/
+        return true;
+    }
 
-require __DIR__.'/../vendor/autoload.php';
+    return false;
+});
