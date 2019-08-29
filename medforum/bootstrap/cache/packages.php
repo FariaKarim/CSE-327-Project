35db2995511f8229802521e470a7b18bdb93a36a
Anika-Hossain-Orthy <52565535+Anika-Hossain-Orthy@users.noreply.github.com>
2019-08-29 13:25:27 +0600

    Add files via upload

---
+++ medforum/bootstrap/cache/packages.php
@@ -0,0 +1,28 @@
+<?php return array (
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+);
